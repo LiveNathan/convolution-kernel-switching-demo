1ceeaa57bfc16ef515cd3cb7fa2e966e800c21d4
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2025-07-18 08:50:45 -0500

    Initial commit

---
+++ src/main/java/dev/nathanlively/convolution_kernel_switching_demo/ConvolutionKernelSwitchingDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.convolution_kernel_switching_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvolutionKernelSwitchingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvolutionKernelSwitchingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nathanlively/convolution_kernel_switching_demo/ConvolutionKernelSwitchingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.convolution_kernel_switching_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvolutionKernelSwitchingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
